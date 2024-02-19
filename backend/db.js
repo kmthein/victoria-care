b68574c71b4e691538dbceeed600afb75bba136d
kmthein <kmthein96.mm@gmail.com>
2024-02-19 13:46:59 +0630

    update fix

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,14 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "victoria-care"
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
