96d1bbb62459e367f194605d014b40ce0aad48b7
kmthein <kmthein96.mm@gmail.com>
2024-02-20 11:25:56 +0630

    update

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const db = mysql.createPool({
+export const db = mysql.createConnection({
     host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT,
 })
